11ef3884e041992e0a4cc536ae3ee46b6a4ffb3a
jenifer_yang <jenifer@jenifer-yangdeiMac.local>
2018-05-16 14:23:38 +0800

    init project structure

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,11 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
-
+require('styles/App.scss');
 import React from 'react';
-
 //获取图片相关数据
-let imageDatas = require('../data/imageDatas.json');
+let imageDatas = require('json!../data/imageDatas.json');
 
 //利用自执行函数，将图片名信息转化为图片URL路径信息
-imageDatas = (function genImageURL(imageDatasArr) {
+imageDatas = ((imageDatasArr) => {
 	for(let i = 0; i < imageDatasArr.length; i++){
 		let singleImageData = imageDatasArr[i];
 		singleImageData.imageURL = require('../images/'+ singleImageData.fileName);
@@ -16,36 +14,19 @@ imageDatas = (function genImageURL(imageDatasArr) {
 	return imageDatasArr;
 })(imageDatas);
 
-var GalleryByReactApp = React.CreateClass({
-	render: function(){
+class GalleryByReactApp extends React.Component {
+	render() {
 		return (
-			<section className="stage">
-				<section className="img-sec">
-				</section>
-				<nav class="controller-nav">
-				</nav>
-			</section>
-		);
+	      	<section className="stage">
+	        	<section className="img-sec">
+	        	</section>
+	        	<nav className="controller-nav">
+	        	</nav>
+	      	</section>
+	    );
 	}
-});
-
-React.render(
-	<GalleryByReactApp/>,
-	document.getElementById('content')
-);
-
-class AppComponent extends React.Component {
-  render() {
-    return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
-    );
-  }
-}
-
-AppComponent.defaultProps = {
 };
 
-export default AppComponent;
+GalleryByReactApp.defaultProps = {};
+
+export default GalleryByReactApp;
